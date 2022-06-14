6ef06c83c4915c5dcc1d6b04b6b27752344166df
YeongJun Seo <ay9564@naver.com>
2022-06-15 03:36:38 +0900

    Refactor : AppConfig 리팩토링 -> memberRepository, discountPolicy 의존성 주입

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,6 +1,9 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
+import hello.core.member.domain.MemberRepository;
 import hello.core.member.domain.MemoryMemberRepository;
 import hello.core.member.service.MemberService;
 import hello.core.member.service.MemberServiceImpl;
@@ -10,10 +13,20 @@ import hello.core.order.service.OrderServiceImpl;
 public class AppConfig {
 
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
+
+    public DiscountPolicy discountPolicy() {
+//        return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
+    }
+
 }
