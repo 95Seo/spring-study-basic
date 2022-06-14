9032238e70c823ebe85beae72e97df094000b2c4
YeongJun Seo <ay9564@naver.com>
2022-06-15 05:12:07 +0900

    Feat : ApplicationContext의 Bean 조회하는 방법 예제

---
+++ src/test/java/hello/core/beanfind/ApplicationContextBasicFindTest.java
@@ -0,0 +1,49 @@
+package hello.core.beanfind;
+
+import hello.core.AppConfig;
+import hello.core.member.service.MemberService;
+import hello.core.member.service.MemberServiceImpl;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ApplicationContextBasicFindTest {
+
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+    @Test
+    @DisplayName("빈 이름으로 조회")
+    void findBeanByName() {
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    @Test
+    @DisplayName("이름 없이 타입으로만 조회")
+    void findBeanByType() {
+        MemberService memberService = ac.getBean(MemberService.class);
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    // 항상 역할과 구현을 구분해야한다.
+    // 역할에 의존해야한다.
+    // 그렇기 때문에 구현체로 의존하는건 좋지않음.
+    @Test
+    @DisplayName("구체 타입으로 조회")
+    void findBeanByName2() {
+        MemberService memberService = ac.getBean("memberService", MemberServiceImpl.class);
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    @Test
+    @DisplayName("빈 이름으로 조회x")
+    void findBeanByNameX() {
+//        ac.getBean("memberServiceX", MemberService.class);
+        assertThrows(NoSuchBeanDefinitionException.class,
+                () -> ac.getBean("memberServiceX", MemberService.class));
+    }
+}
--- src/test/java/hello/core/beanfind/ApplicationContextInfoTest.java
+++ src/test/java/hello/core/beanfind/ApplicationContextInfoTest.java
@@ -16,7 +16,7 @@ public class ApplicationContextInfoTest {
         String[] beanDefinitionNames = ac.getBeanDefinitionNames();
         for (String beanDefinitionName : beanDefinitionNames) {
             Object bean = ac.getBean(beanDefinitionName);
-            System.out.println("name = " + beanDefinitionName + " object = " + bean);
+            System.out.println("name = " + beanDefinitionName + ", object = " + bean);
         }
     }
 
@@ -32,7 +32,7 @@ public class ApplicationContextInfoTest {
             // Role ROLE_APPLICATION : 직접 등록한 애플리케이션 빈
             if (beanDefinition.getRole() == BeanDefinition.ROLE_APPLICATION) {
                 Object bean = ac.getBean(beanDefinitionName);
-                System.out.println("name = " + beanDefinitionName + " object = " + bean);
+                System.out.println("name = " + beanDefinitionName + ", object = " + bean);
             }
         }
     }
@@ -47,7 +47,7 @@ public class ApplicationContextInfoTest {
             // Role ROLE_INFRASTRUCTURE : 스프링이 내부에서 사용하는 빈
             if (beanDefinition.getRole() == BeanDefinition.ROLE_INFRASTRUCTURE) {
                 Object bean = ac.getBean(beanDefinitionName);
-                System.out.println("name = " + beanDefinitionName + " object = " + bean);
+                System.out.println("name = " + beanDefinitionName + ", object = " + bean);
             }
         }
     }
